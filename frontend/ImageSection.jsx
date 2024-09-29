4a9aeb532940e06bd27b6ea77ad9853fef6a6643
520gaby <155792041+520gaby@users.noreply.github.com>
2024-09-28 23:20:15 -0400

    Update ImageSection.jsx

--- frontend/ImageSection.jsx
+++ frontend/ImageSection.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
-function ImageSection() {
+const ImageSection = () => {
   return (
     <section className="flex flex-col w-[44%] max-md:ml-0 max-md:w-full">
       <div className="flex flex-col justify-center px-2.5 py-3 w-full rounded-md bg-slate-700 max-md:mt-10 max-md:max-w-full">
-        <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/6c95ef55b819cccb48a4b46d4b56c5ec7fb936cca9afb3e5774187de796ff282?placeholderIfAbsent=true&apiKey=8334832cff474ef69011196ad1797df9" alt="" className="object-contain w-full rounded-md aspect-[1.5] max-md:max-w-full" />
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/6c95ef55b819cccb48a4b46d4b56c5ec7fb936cca9afb3e5774187de796ff282?placeholderIfAbsent=true&apiKey=8334832cff474ef69011196ad1797df9"
+          alt="Sample Image"
+          className="object-contain w-full rounded-md aspect-[1.5] max-md:max-w-full"
+        />
       </div>
     </section>
   );
-}
+};
 
 export default ImageSection;
